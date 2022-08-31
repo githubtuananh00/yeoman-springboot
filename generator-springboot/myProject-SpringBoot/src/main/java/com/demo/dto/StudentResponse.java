dfc1df4f5981dce14e1e183b4a3bfbe113c91ab9
Tuan Anh <laituananh22k@gmail.com>
2022-08-31 08:54:34 +0700

    Add project demo

---
+++ generator-springboot/myProject-SpringBoot/src/main/java/com/demo/dto/StudentResponse.java
@@ -0,0 +1,18 @@
+package com.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class StudentResponse {
+    // private Long id;
+    // private String text;
+    
+    private String Name;
+    
+    
+    private int Age;
+    
+    
+    private String Address;
+    
+}
---
+++ generator-springboot/myProject-SpringBoot/src/main/java/com/demo/services/StudentService.java
@@ -0,0 +1,37 @@
+package com.demo.services;
+
+import com.demo.entities.Student;
+import com.demo.repositories.StudentRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> findAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Optional<Student> findStudentById(Long id) {
+        return studentRepository.findById(id);
+    }
+
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudentById(Long id) {
+        studentRepository.deleteById(id);
+    }
+}
